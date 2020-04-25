0a96b4ee6aaa9585ce60d459b349278e29e6c34a
mayconxhh <mayconxhh@gmail.com>
2020-04-24 20:26:14 -0500

    se cambio archivos minificados

---
+++ precache-manifest.7274aaabf3b57ae2945165c5f3dfa077.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681c7fae258c9d9ab9e106c2822e2976",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30b36e47a15b492e5e1c",
+    "url": "/static/css/main.ed34413a.chunk.css"
+  },
+  {
+    "revision": "ff4072a8f679d95e96a9",
+    "url": "/static/js/2.28e1c5d4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.28e1c5d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30b36e47a15b492e5e1c",
+    "url": "/static/js/main.120eaf7f.chunk.js"
+  },
+  {
+    "revision": "690f225c668be9ff41d9",
+    "url": "/static/js/runtime-main.af7f4d6c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
